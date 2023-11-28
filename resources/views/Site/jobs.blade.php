60daf30505de1ed189df08f300032e871fe7c709
RodrigoNasct <rodrigonasct13@gmail.com>
2023-11-28 06:54:50 -0300

    jobs repagination

--- resources/views/Site/jobs.blade.php
+++ resources/views/Site/jobs.blade.php
@@ -5,52 +5,119 @@
 
 @section('content')
 
-    <link rel="stylesheet" href="{{ asset('css/jobs_page.css') }}">
+    <link rel="stylesheet" href="{{ asset('css/jobs-page.css') }}">
 
     <main>
 
-        <div id="text-span">
+        <div class="filter-area container ">
 
-            <span>Vagas Disponíves</span>
+            <form class="d-flex" action="">
+
+                <input class="form-control flex-grow-1" type="text" name="" id="">
+
+            </form>
 
         </div>
 
-        <div id="jobs-division">
+        <div class="text-area container text-center">
 
-            @foreach ($vagas as $vaga)
+            <h1 class="display-5">Vagas Disponíves</h1>
 
-                <div class="job-card">
+            <div class="dropdown">
 
-                    <div id="text-area-card">
+                <button class="dropdown-toggle btn btn-large" data-bs-toggle="dropdown" aria-expanded="false">Mais filtros</button>
+                
+                <div class="dropdown-menu dropdown-menu-right" aria-labelledby="dropdownMenuButton">
 
-                        <h2>{{ $vaga->titulo }}</h2>
-                        <h3>{{ $vaga->empresa->nm_fantasia }}</h3>
-                        <h4>{{ $vaga->modalidade->nome }}({{ $vaga->periodo->nome }}) </h4>
+                    <div class="filters container">
+
+                        <form action="">
+
+                            <div class="form-group text-center mb-3 mt-4">
+
+                                <h2>Período de estágio:</h2>
+
+                                <div class="d-flex flex-wrap">
+
+                                    <div class="form-norm">
+
+                                        <input type="checkbox" class="check-input form-check-input" id="dropdownCheck">
+                                        <label class="form-check-label h3" for="dropdownCheck">Matutino</label>
+
+                                    </div>
+                                    
+                                    <div>
+
+                                        <input type="checkbox" class="check-input form-check-input" id="dropdownCheck">
+                                        <label class="form-check-label h3" for="dropdownCheck">Vespertino</label>
+
+                                    </div>
+
+                                    <div>
+
+                                        <input type="checkbox" class="check-input form-check-input" id="dropdownCheck">
+                                        <label class="form-check-label h3" for="dropdownCheck">Noturno</label>
+
+                                    </div>
+                                
+                                </div>
+
+                            </div>
+
+                            <div class="form-group text-center">
+
+                                <h2>Local de trabalho:</h2>
+
+                                <div class="d-flex flex-wrap">
+
+                                    <div class="form-norm">
+
+                                        <input type="checkbox" class="check-input form-check-input" id="dropdownCheck">
+                                        <label class="form-check-label h3" for="dropdownCheck">Remoto</label>
+
+                                    </div>
+                                    
+                                    <div>
+
+                                        <input type="checkbox" class="check-input form-check-input" id="dropdownCheck">
+                                        <label class="form-check-label h3" for="dropdownCheck">Híbrido</label>
+
+                                    </div>
+
+                                    <div>
+
+                                        <input type="checkbox" class="check-input form-check-input" id="dropdownCheck">
+                                        <label class="form-check-label h3" for="dropdownCheck">Presencial</label>
+
+                                    </div>
+                                
+                                </div>
+
+                            </div>
+
+                            <div class="form-group">
+                            <label for="exampleDropdownFormPassword1">Password</label>
+                            <input type="password" class="form-control" id="exampleDropdownFormPassword1" placeholder="Password">
+                            </div>
+                            <div class="form-check">
+
+                            </div>
+                            <button type="submit" class="btn btn-primary">Sign in</button>
+
+                        </form>
 
                     </div>
 
-                    <div id="image-area-card">
 
-                        <img src="{{ $ajuste_vaga ? '../' : '' }}{{ $vaga->empresa->user->nm_img }}" alt="{{ $ajuste_vaga ? '../' : '' }}{{ $vaga->empresa->user->nm_img }}">
-
-                    </div>
-
-                    <div id="interaction-area-card">
-
-                        <h2>R$ {{ str_replace('.', ',', $vaga->salario) }}  (Bruto mensal)</h2>
-                        
-                        <div>
-
-                            <h4>Publicada em {{ (new DateTime($vaga->created_at))->format('d/m/Y') }}</h4>
-                            <button>Cadastrar-se</button>
-
-                        </div>
-
-                    </div>
 
                 </div>
+            </div>
 
-            @endforeach
+        </div>
+
+        <div class="jobs-area container">
+
+
 
         </div>
 
